4adb9cd114d78a914dc233a305cfb421a8dc4c15
Nicolas Johnson <nicolasssousa2011@gmail.com>
2024-05-07 15:22:29 -0300

    FAQs Done

--- FAQs/main.js
+++ FAQs/main.js
@@ -1,4 +1,22 @@
 import { data } from './data.js';
-/*
- <details>/<summary> 
- */
+
+const article = document.querySelector('.wrapper');
+
+const createFaqs = (faq) => {
+    const { question, answer } = faq;
+    const faqElement = document.createElement('details');
+    const faqQuestion = document.createElement('summary');
+    const faqAnswer = document.createElement('p');
+    faqAnswer.innerHTML = `${answer}`;
+    faqQuestion.innerHTML = `${question}`;
+    faqElement.appendChild(faqQuestion);
+    faqElement.appendChild(faqAnswer);
+
+    return faqElement;
+};
+
+const apendQuestions = () => {
+    data.forEach(faq => article.appendChild(createFaqs(faq)));
+};
+
+window.addEventListener('load', () => apendQuestions());
