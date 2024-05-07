1873b043516358ff6d461c1ffa752aad7fec1df2
Nicolas Johnson <nicolasssousa2011@gmail.com>
2024-05-07 15:09:46 -0300

    Start of FAQs

---
+++ FAQs/data.js
@@ -0,0 +1,34 @@
+export const data = [
+    {
+        question: 'COMO FUNCIONA A SCP - Sociedade em Conta de Participação',
+        answer: 'Cada cota representa não apenas um investimento, mas também a confiança mútua que nos guia na busca por oportunidades financeiras. A estrutura da Sociedade em Conta de Participação permite que os sócios participem de forma específica em termos de responsabilidade, administração e distribuição de lucros. Na Youniit.y, atuamos como Sócio Ostensivo, assumindo a responsabilidade  pelas escolhas de investimento, pela gestão das melhores oportunidades para nossos investidores e também por toda a responsabilidade legal sobre as operações. Como Sócio Participante, você investe capital de forma anônima, sem responsabilidades perante terceiros.'
+    },
+    {
+        question: 'POR QUE OS LUCROS SÃO ISENTOS DE IMPOSTO DE RENDA?',
+        answer: 'Os lucros obtidos em nossos investimentos são tratados como dividendos da empresa, beneficiando-se do formato de Sociedade em Cotas de Participação. Essa estrutura permite que os lucros não sejam tributados, incentivando o reinvestimento e o crescimento empresarial.'
+    },
+    {
+        question: 'PORQUE DIVERSIFICAR SEUS INVESTIMENTOS?',
+        answer: 'Diversificar os investimentos é essencial para mitigar riscos e potencializar retornos. Investindo em setores variados como imobiliário, agronegócio, franquias e energia renovável, garantimos estabilidade e aproveitamos oportunidades lucrativas em diferentes mercados, seguindo uma estratégia sólida para o sucesso financeiro.'
+    },
+    {
+        question: 'QUAL A DIFERENÇA ENTRE INVESTIMENTOS EM COTAS IMOBILIÁRIAS E UNIDADES IMOBILIÁRIAS?',
+        answer: 'As cotas imobiliárias oferecem uma forma mais acessível e diversificada de investir no setor imobiliário, permitindo aos investidores participarem de empreendimentos por meio de fundos de investimento imobiliário (FII) ou projetos específicos, com retornos proporcionais à participação nas cotas e pagamentos regulares com base na performance do fundo ou empreendimento. Por outro lado, as unidades imobiliárias proporcionam aos investidores uma participação direta em propriedades específicas, com potencial de valorização e retornos provenientes da valorização do imóvel ao longo do tempo, aluguel ou ambos. Os investidores têm mais controle sobre a gestão do imóvel, incluindo decisões sobre aluguel, manutenção e venda, porém a liquidez pode ser menor em comparação com as cotas de FII, devido ao processo mais complexo de venda de um imóvel. A escolha entre os dois tipos de investimento dependerá da estratégias que traçaremos baseado no perfil de investimento e retornos possiveis.'
+    },
+    {
+        question: 'COMO SÃO DEFINIDAS AS ESTRATÉGIAS DE INVESTIMENTO?',
+        answer: 'Realizamos uma meticulosa análise de mercado para identificar oportunidades e tendências, bem como análise legal, operacional e financeira, das possibilidades de investimentos. Além disso, nossa gestão de portfólio é diversificada e muito estudada para garantir rentabilidade e reduzir riscos. Levamos em consideração também análises de risco e garantimos conformidade com regulamentações e normas de compliance. Essa abordagem integrada nos permite desenvolver estratégias sólidas e eficazes para aportes de capital.'
+    },
+    {
+        question: 'QUAIS OS RISCOS ENVOLVIDOS NA OPERAÇÃO?',
+        answer: 'Os riscos associados às nossas operações podem variar de acordo com o setor de investimento. Contudo, contamos com uma equipe de assessoria jurídica especializada para realizar uma análise minuciosa dos contratos e empresas envolvidas, garantindo a mitigação eficaz dos riscos. Ao apresentar uma proposta de investimento, oferecemos uma exposição transparente e detalhada dos riscos inerentes a cada setor, assegurando uma compreensão abrangente ao investidor.'
+    },
+    {
+        question: 'EM QUANTO TEMPO POSSO ESPERAR RETORNO DOS MEUS INVESTIMENTOS?',
+        answer: 'Você pode esperar retornos dos seus investimentos mensalmente. Nossos pagamentos variam entre 1,2% e 1,5% ao mês, com prazos de devolução do capital investido de 12, 24 ou 36 meses.'
+    },
+    {
+        question: 'COMO POSSO COMEÇAR A INVESTIR?',
+        answer: 'Comece sua jornada de investimento com nosso atendimento extremamente personalizado. Conheça a estrutura da nossa empresa, escute uma proposta de rentabilidade que atenda aos seus anseios, planos e desejos. Entenda todos os riscos e benefícios antes de assinar o contrato e começar a colher os resultados. Pronto para começar? Clique aqui para entrar em contato conosco e dar o primeiro passo rumo ao seu futuro financeiro.'
+    }
+];
